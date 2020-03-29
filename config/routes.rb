09b99481fa4a844d338e15bb0cab2380c860ddad
wayne712 <weilong712@gmail.com>
2020-03-29 15:50:44 +0800

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :center do
+    root 'acquires#index'
     resources :sessions, only: [:create, :new] do
       delete :destroy, on: :collection
     end
