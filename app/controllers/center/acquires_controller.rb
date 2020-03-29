e9d3df371d13f0781947904a23e7f2124b2b1e28
wayne712 <weilong712@gmail.com>
2020-03-29 15:55:35 +0800

    Update

--- app/controllers/center/acquires_controller.rb
+++ app/controllers/center/acquires_controller.rb
@@ -1,66 +1,68 @@
-class Center::AcquiresController < ApplicationController
+module Center
+  class AcquiresController < BaseController
 
-  # GET /acquires
-  # GET /acquires.json
-  def index
-    @acquires = Acquire.paginate(page: params[:page]).order(id: :desc)
-  end
+    # GET /acquires
+    # GET /acquires.json
+    def index
+      @acquires = Acquire.paginate(page: params[:page]).order(id: :desc)
+    end
 
-  # GET /acquires/1
-  # GET /acquires/1.json
-  def show
-  end
+    # GET /acquires/1
+    # GET /acquires/1.json
+    def show
+    end
 
-  # GET /acquires/new
-  def new
-    @acquire = Acquire.new
-  end
+    # GET /acquires/new
+    def new
+      @acquire = Acquire.new
+    end
 
-  # GET /acquires/1/edit
-  def edit
-  end
+    # GET /acquires/1/edit
+    def edit
+    end
 
-  # POST /acquires
-  # POST /acquires.json
-  def create
-    @acquire = Acquire.new(acquire_params)
+    # POST /acquires
+    # POST /acquires.json
+    def create
+      @acquire = Acquire.new(acquire_params)
 
-    respond_to do |format|
-      if @acquire.save
-        format.html { redirect_to [:center, @acquire], notice: 'Acquire was successfully created.' }
-        format.json { render :show, status: :created, location: @acquire }
-      else
-        format.html { render :new }
-        format.json { render json: @acquire.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @acquire.save
+          format.html { redirect_to [:center, @acquire], notice: 'Acquire was successfully created.' }
+          format.json { render :show, status: :created, location: @acquire }
+        else
+          format.html { render :new }
+          format.json { render json: @acquire.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  # PATCH/PUT /acquires/1
-  # PATCH/PUT /acquires/1.json
-  def update
-    respond_to do |format|
-      if @acquire.update(acquire_params)
-        format.html { redirect_to [:center, @acquire], notice: 'Acquire was successfully updated.' }
-        format.json { render :show, status: :ok, location: @acquire }
-      else
-        format.html { render :edit }
-        format.json { render json: @acquire.errors, status: :unprocessable_entity }
+    # PATCH/PUT /acquires/1
+    # PATCH/PUT /acquires/1.json
+    def update
+      respond_to do |format|
+        if @acquire.update(acquire_params)
+          format.html { redirect_to [:center, @acquire], notice: 'Acquire was successfully updated.' }
+          format.json { render :show, status: :ok, location: @acquire }
+        else
+          format.html { render :edit }
+          format.json { render json: @acquire.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  # DELETE /acquires/1
-  # DELETE /acquires/1.json
-  def destroy
-    @acquire.destroy
-    respond_to do |format|
-      format.html { redirect_to center_acquires_url, notice: 'Acquire was successfully destroyed.' }
-      format.json { head :no_content }
+    # DELETE /acquires/1
+    # DELETE /acquires/1.json
+    def destroy
+      @acquire.destroy
+      respond_to do |format|
+        format.html { redirect_to center_acquires_url, notice: 'Acquire was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
-  end
 
-  private
+    private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_acquire
       @acquire = Acquire.find(params[:id])
@@ -70,4 +72,5 @@ class Center::AcquiresController < ApplicationController
     def acquire_params
       params.fetch(:acquire, {})
     end
+  end
 end
